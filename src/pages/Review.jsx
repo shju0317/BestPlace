1aaf895487dd486db20a10f1d5ca89be6ff68214
shju0317 <shju0317@naver.com>
2023-09-07 22:47:19 +0900

    취소 버튼 추가

--- src/pages/Review.jsx
+++ src/pages/Review.jsx
@@ -1,18 +1,24 @@
-import ReviewKeyword from '@/components/review/ReviewKeyword';
-import Button from '@/components/review/Button';
-import VisitedPlace from '@/components/review/VisitedPlace';
-import ReviewInput from '@/components/review/ReviewInput';
-import ReviewPhoto from '@/components/review/ReviewPhoto';
+import ReviewKeyword from '@c/review/ReviewKeyword';
+import Button from '@c/review/Button';
+import VisitedPlace from '@c/review/VisitedPlace';
+import ReviewInput from '@c/review/ReviewInput';
+import ReviewPhoto from '@c/review/ReviewPhoto';
+import { BiX } from "react-icons/bi";
+import { MdOutlineCancel } from "react-icons/md";
 
 function Review() {
   return (
+    <>
+    {/* <BiX className="w-7 h-7 text-primary float-right"/> */}
     <form className="flex flex-col gap-4 flex-wrap mx-auto max-w-3xl p-3">
+      <button type="button" aria-label="작성취소"><MdOutlineCancel className="w-7 h-7 text-primary float-right ml-auto"/></button>
       <VisitedPlace/>
       <ReviewInput/>
       <ReviewPhoto/>
       <ReviewKeyword/>
       <Button type='submit' text='등록하기'/>
     </form>
+    </>
   )
 }
 
