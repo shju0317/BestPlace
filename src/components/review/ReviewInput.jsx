ee544ba47061785f48dc35372fd9122e68d6778b
shju0317 <shju0317@naver.com>
2023-09-07 15:23:11 +0900

    입력된 글자수 표시

--- src/components/review/ReviewInput.jsx
+++ src/components/review/ReviewInput.jsx
@@ -1,16 +1,19 @@
-import { useId } from "react"
+import { useId, useState } from "react"
+
 
 function ReviewInput() {
   const inputId = useId();
+  const [letterCount, setLetterCount] = useState(0);
 
   return (
     <div className="flex flex-col gap-2 w-full self-center">
       <label htmlFor="inputId" className="text-lg text-center font-semibold">리뷰를 남겨주세요</label>
-      <textarea id="inputId" cols="20" rows="5"  
+      <textarea id="inputId" rows="5" maxLength="400"
         placeholder="업주와 다른 사용자들이 상처받지 않도록 좋은 표현을 사용해주세요. 유용한 Tip도 남겨주세요!"
-        className="bg-slate-100 rounded p-3">
+        className="bg-slate-100 rounded p-3 focus:outline-primary"
+        onChange={(e)=>setLetterCount(e.target.value.length)}>
       </textarea>
-      <span className="text-xs text-right">0/400</span>
+      <span className="text-xs text-right">{letterCount}/400</span>
     </div>
   )
 }
