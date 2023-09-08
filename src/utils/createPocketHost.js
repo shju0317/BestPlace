4ab7bc40838a65be083e5a84488b957c9efdf6b5
민규 <128453503+Engsheet@users.noreply.github.com>
2023-09-08 15:33:46 +0900

    comment : createPocketHost.js 삭제 필요한 코드 주석 추가

--- src/utils/createPocketHost.js
+++ src/utils/createPocketHost.js
@@ -3,6 +3,7 @@ import PocketBase from "pocketbase";
 async function create(collection, data) {
   const pb = new PocketBase("https://lionplace-db.pockethost.io/");
   const create = await pb.collection(collection).create(data);
+  // 추후 삭제해야 함
   console.log('created data on collection');
   return create;
 }
