5288c99b4b2c060ac2803c442d96b975a0b64594
shju0317 <shju0317@naver.com>
2023-09-11 14:37:11 +0900

    modify: 불필요한 코드 수정
    ```bash
    타입 : 내용(#issueNumber)
    
    ```
    
    - **fix** : 버그 및 에러 수정에 관한 커밋 메시지
    - **add** : 파일 추가에 관한 커밋 메시지
    - **delete** : 파일 삭제에 관한 커밋 메시지
    - **docs** : 문서화에 관련된 커밋 메시지
    - **feat** : 새로운 기능 추가에 관련된 커밋 메시지
    - **modify** : 파일, 폴더, 코드 수정 시 커밋 메시지
    - **refactor** : 코드 고도화에 관련된 커밋 메시지
    - **comment** : 주석 추가
    - **setting** : 세팅 관련된 커밋 메시지
    
    - 메세지 타입은 영어로, 내용은 한글로 작성<br>
        ex) modify : ㅇㅇ 기능 코드 수정(#issueNumber)<br>
    - 팀원들이 이해하기 쉽게 직관적으로 작성할 것
    - 커밋 메세지는 3줄 이내로 작성할 것
    - 남겨야할 메세지가 길 경우에는 Git Issue 사용
    - PR 전에 분할된 커밋은 rebase로 합치기

--- src/components/review/VisitedPlace.jsx
+++ src/components/review/VisitedPlace.jsx
@@ -1,22 +1,5 @@
-// const visitedPlaces = [
-//   {
-//     id: 'visited-1',
-//     name: '투썸플레이스 상무번영로점',
-//     subject: '카페',
-//     location: '광주광역시 서구 치평동'
-//   },
-//   {
-//     id: 'visited-2',
-//     name: '스테이_STAY',
-//     subject: '양식',
-//     location: '광주광역시 북구 중흥동'
-//   }
-// ]
-
-// import { read, create } from "@/utils";
-
 import { useEffect, useState } from 'react';
-import { readRecordList, readRecordOne } from '@u/readRecordList'
+import { readRecordOne } from '@u/readRecordList'
 
 function VisitedPlace() {
 
