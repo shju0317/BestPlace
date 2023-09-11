5bd50275db83f7f1037fea5fae4cce831a4c69da
shju0317 <shju0317@naver.com>
2023-09-11 10:05:56 +0900

    modify: 리뷰페이지 사진 추가 기능 수정(#27)
    ```bash
    타입 : 내용(#issueNumber)
    
    ```
    
    - **fix** : 버그 및 에러 수정에 관한 커밋 메시지
    - **add** : 파일 추가에 관한 커밋 메시지
    - **delete** : 파일 삭제에 관한 커밋 메시지
    - **docs** : 문서화에 관련된 커밋 메시지
    - **feat** : 새로운 기능 추가에 관련된 커밋 메시지
    - **modify** : 파일, 폴더, 코드 수정 시 커밋 메시지
    - **refactor** : 코드 고도화에 관련된 커밋 메시지
    - **comment** : 주석 추가
    - **setting** : 세팅 관련된 커밋 메시지
    
    - 메세지 타입은 영어로, 내용은 한글로 작성<br>
        ex) modify : ㅇㅇ 기능 코드 수정(#issueNumber)<br>
    - 팀원들이 이해하기 쉽게 직관적으로 작성할 것
    - 커밋 메세지는 3줄 이내로 작성할 것
    - 남겨야할 메세지가 길 경우에는 Git Issue 사용
    - PR 전에 분할된 커밋은 rebase로 합치기

--- src/components/review/ReviewPhoto.jsx
+++ src/components/review/ReviewPhoto.jsx
@@ -1,8 +1,11 @@
 import { CiImageOn } from "react-icons/ci";
-import { PiPlusCircle } from "react-icons/pi";
+import { PiPlusCircle} from "react-icons/pi";
+import { MdOutlineCancel } from "react-icons/md";
 import { useRef, useState } from 'react';
 
 function ReviewPhoto() {
+  const MAX_IMAGE_COUNT = 10;
+
   const [fileImages, setFileImages] = useState([]);
   const [imageCount, setImageCount] = useState(0);
   const photoRef = useRef(null);
@@ -15,25 +18,63 @@ function ReviewPhoto() {
 
   const showElement = (element) => {
     element.current.style.display = "flex";
-    console.log(element);
   }
+
+  // const handleUpload = (e) => {
+  //   const { files } = e.target;
+
+  //   const fileImages = Array.from(files).map((file) => ({
+  //     image: URL.createObjectURL(file),
+  //     label: file.name,
+  //   }));
+
+  //   setImageCount(files.length);
+  //   setFileImages(fileImages);
+
+  //   if(files.length <= 0){
+  //     showElement(labelRef);
+  //     hideElement(divRef);
+  //   }else{
+  //     showElement(divRef);
+  //     hideElement(labelRef);
+  //   }
+  // };
 
   const handleUpload = (e) => {
     const { files } = e.target;
-    console.log('file길이래',files.length);
-    const fileImages = Array.from(files).map((file) => ({
+  
+    const newFileImages = Array.from(files).map((file) => ({
       image: URL.createObjectURL(file),
       label: file.name,
     }));
+  
+    const updatedFileImages = [...fileImages, ...newFileImages];
+    const updatedImageCount = imageCount + files.length;
 
-    console.log(fileImages);
-    setImageCount(files.length);
-    setFileImages(fileImages);
+    if (files.length + fileImages.length > MAX_IMAGE_COUNT) {
+      return; // 이미지 개수가 최대 값을 초과하면 업로드 중단
+    }else{
+      setFileImages(updatedFileImages);
+      setImageCount(updatedImageCount);
+    }
 
-    showElement(divRef);
-    hideElement(labelRef);
-
+    if (updatedImageCount <= 0) {
+      showElement(labelRef);
+      hideElement(divRef);
+    } else {
+      showElement(divRef);
+      hideElement(labelRef);
+    }
   };
+  
+  const handleDeleteImage = (index) => {
+    setFileImages((prevFileImages) =>
+      prevFileImages.filter((_, i) => i !== index)
+    );
+    setImageCount((prevImageCount) => prevImageCount -1 );
+  };
+
+
 
   return (
     <div className="flex flex-col gap-2 w-full">
@@ -42,7 +83,7 @@ function ReviewPhoto() {
         className="flex justify-center items-center gap-1 py-1 border border-primary rounded font-semibold w-full self-center">
         <CiImageOn />
         사진추가
-        <span className="text-slate-400 text-xs">최대 10장</span>
+        <span className="text-slate-400 text-xs">최대 {MAX_IMAGE_COUNT}장</span>
       </label>
       <div className="relative">
         <input
@@ -57,17 +98,20 @@ function ReviewPhoto() {
         <div className="hidden flex gap-4" ref={divRef}>
         <div 
           className="flex border border-primary rounded gap-2 overflow-x-auto p-2 h-36 w-full"
-          
         >
-          {fileImages.map((file) => {
-            return (
-              <img key={file.label} src={file.image} alt={file.label} />
-            );
-          })}
+        {fileImages.map((file,index)=> (
+              <div key={index} className="relative">
+                <img src={file.image} alt={file.label} />
+                {/* 삭제 버튼 */}
+                <button onClick={() => handleDeleteImage(index)} className='absolute top-[4px] right-[4px] text-primary' aria-label="이미지 삭제">
+                  <MdOutlineCancel size="20"/>
+                </button>
+              </div>
+            ))}
         </div>
         <div className="flex flex-col justify-center items-center text-primary">
           <PiPlusCircle className="w-20 h-20"/>
-          <p className="font-bold">{imageCount}/20</p>
+          <p className="font-bold">{imageCount}/{MAX_IMAGE_COUNT}</p>
         </div>
         </div>
       </div>
