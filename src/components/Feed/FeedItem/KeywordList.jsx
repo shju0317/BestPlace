b16397810115ffcea45cd8a4338e3565d031b7ae
cfgop23 <cfgop23@gmail.com>
2023-09-12 21:38:42 +0900

    feat : 피드페이지 기능 구현 - 카테고리 필터 기능, 피드 데이터 정렬, 사진 레이아웃, 컴포넌트 분리, 전역 상태 괸리(#23)

---
+++ src/components/Feed/FeedItem/KeywordList.jsx
@@ -0,0 +1,11 @@
+import { string } from "prop-types";
+
+function KeywordList({ item }) {
+  return <li className="w-fit rounded-md bg-gray-100 px-2 py-1 text-sm text-gray-500">{item}</li>;
+}
+
+KeywordList.propTypes = {
+  item: string,
+};
+
+export default KeywordList;
--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -5,6 +5,7 @@ import { node } from "prop-types";
 function RootLayout({ children }) {
   return (
     <div>
+      <h1 className="sr-only">베스트 플레이스</h1>
       <Header />
       <Nav />
       <main className="mx-auto max-w-3xl p-3">{children}</main>
