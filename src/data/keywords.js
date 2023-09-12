dad192eb988d674982774f7e19597085e259ee78
shju0317 <shju0317@naver.com>
2023-09-12 22:19:33 +0900

    modify: immer 사용하여 리뷰 키워드 버튼 클릭시 색상변경 기능 리팩토링, 키워드 파일 분리(data/keywrods.js)(#27)

---
+++ src/data/keywords.js
@@ -0,0 +1,54 @@
+const KEYWORDS = [
+	{
+    id: 'keyword-1',
+    name: '친절해요',
+    emoji: '😊'
+  },
+	{
+    id: 'keyword-2',
+    name: '매장이 넓어요',
+    emoji: '🏢'
+  },
+  {
+    id: 'keyword-3',
+    name: '매장이 청결해요',
+    emoji: '✨'
+  },
+  {
+    id: 'keyword-4',
+    name: '인테리어가 멋져요',
+    emoji: '🌆'
+  },
+  {
+    id: 'keyword-5',
+    name: '뷰가 좋아요',
+    emoji: '👀'
+  },
+  {
+    id: 'keyword-6',
+    name: '맛있어요',
+    emoji: '🤩'
+  },
+  {
+    id: 'keyword-7',
+    name: '양이 많아요',
+    emoji: '🍚'
+  },
+  {
+    id: 'keyword-8',
+    name: '가성비가 좋아요',
+    emoji: '💰'
+  },
+  {
+    id: 'keyword-9',
+    name: '주차하기 편해요',
+    emoji: '🚗'
+  },
+  {
+    id: 'keyword-10',
+    name: '냉난방이 잘돼요',
+    emoji: '🌡'
+  }
+]
+
+export default KEYWORDS
