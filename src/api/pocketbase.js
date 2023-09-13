5614440939e7bb16e63b565af86d29c87206b8cf
bigCoDult <qqqqmmss11@gmail.com>
2023-09-13 17:56:51 +0900

    feat : update 함수 생성, api관련 함수들을 utils에서 pocketbase.js로 이동

--- src/api/pocketbase.js
+++ src/api/pocketbase.js
@@ -1,5 +1,36 @@
 import PocketBase from "pocketbase";
 
 const pb = new PocketBase(import.meta.env.VITE_PB_URL);
+pb.autoCancellation(false);
+export default pb
 
-export default pb;
+
+export async function create(collection, data) {
+  const create = await pb.collection(collection).create(data);
+  
+  // 추후 삭제해야 함
+  console.log("created data on collection");
+  
+  return create;
+}
+
+export async function read(collection, field, id) {
+  const FieldData = await pb.collection(collection).getList(1, 10, {
+    fields: field,
+  });
+  const collectionData = await pb.collection(collection).getList(1, 10);
+  
+  // 추후 삭제해야 함
+  console.log(field ? FieldData : collectionData);
+  
+  return field ? FieldData : collectionData;
+}
+
+export function update(collection, itemId, data) {
+  const update = pb.collection(collection).update(itemId, data);
+  
+  // 추후 삭제해야 함
+  console.log("created data on collection");
+  
+  return update
+}
