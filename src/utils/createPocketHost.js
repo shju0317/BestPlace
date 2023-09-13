f4d0d892bdd3d998e9f4c89b570cea4bc887367a
bigCoDult <qqqqmmss11@gmail.com>
2023-09-13 17:58:24 +0900

    delete : pocketbase.js로 이동한 readPocketHost/createPocketHost 파일삭제

--- src/utils/createPocketHost.js
+++
@@ -1,8 +0,0 @@
-export async function create(pb, collection, data) {
-  const create = await pb.collection(collection).create(data);
-
-  // 추후 삭제해야 함
-  console.log("created data on collection");
-
-  return create;
-}
