ee2d0ab12181d8138aed695090a2f6bdc6a7694d
bigCoDult <qqqqmmss11@gmail.com>
2023-09-14 16:54:05 +0900

    feat : 회원가입 페이지에 테일윈드 적용

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,8 +3,10 @@ import { useState } from "react";
 import SignTitle from "@c/SignInUp/SignTitle";
 import SignInput from "@c/SignInUp/SignInput";
 import SignButton from "@c/SignInUp/SignButton";
-
-import { read, create, update } from "@/api/pocketbase";
+import SignForm from "@c/SignInUp/SignForm";
+import SignContents from "@c/SignInUp/SignContents";
+import { pb, read, create, update } from "@/api/pocketbase";
+import SignLogo from "@c/SignInUp/SignLogo";
 
 function Register() {
   const [id, setId] = useState("");
@@ -32,14 +34,13 @@ function Register() {
     avatar: "",
     regions: [],
   };
-  
+
   const userData = {
+    username: id,
+    email: email,
     password: pw,
     passwordConfirm: pwCheck,
-    
-    username: id,
-    nickname: id,
-    email: email,
+
   };
   const updateData = {
     nickname: "업데이트된 닉네임",
@@ -47,32 +48,40 @@ function Register() {
     // email: "ccc@ccc.com"
   };
 
-  
   return (
-    <>
+    <SignContents>
+      <SignLogo />
       <SignTitle value="회원가입" />
-
-      <SignInput labelValue="아이디" ariaText="아이디 입력창" placeHolder="아이디를 입력하세요" inputValue={setId} />
-      <SignInput labelValue="이메일" ariaText="이메일 입력창" placeHolder="이메일을 입력하세요" inputValue={setEmail} />
-      <SignInput
-        labelValue="비밀번호"
-        ariaText="비밀번호 입력창"
-        placeHolder="비밀번호의 길이는 8자리 이상"
-        inputValue={setPw}
+      <SignForm>
+        <SignInput labelValue="아이디" ariaText="아이디 입력창" placeHolder="아이디를 입력하세요" inputValue={setId} />
+        <SignInput
+          labelValue="이메일"
+          ariaText="이메일 입력창"
+          placeHolder="이메일을 입력하세요"
+          inputValue={setEmail}
+        />
+        <SignInput
+          labelValue="비밀번호"
+          ariaText="비밀번호 입력창"
+          placeHolder="비밀번호의 길이는 8자리 이상"
+          inputValue={setPw}
+        />
+        <SignInput
+          labelValue="비밀번호 확인"
+          ariaText="비밀번호 재입력창"
+          placeHolder="비밀번호를 다시 입력하세요"
+          inputValue={setPwCheck}
+        />
+      </SignForm>
+      {/* <SignButton value="업데이트" handleEvent={() => update("users", pb.authStore.model.id, updateData)} /> */}
+      <SignButton
+        value="회원가입"
+        handleEvent={() => create("users", userData)}
+        bgColor="bg-white"
+        textColor="text-black"
       />
-      <SignInput
-        labelValue="비밀번호 확인"
-        ariaText="비밀번호 재입력창"
-        placeHolder="비밀번호를 다시 입력하세요"
-        inputValue={setPwCheck}
-      />
-      <br />
-      <SignButton value="업데이트" handleEvent={() => update("users", pb.authStore.model.id, updateData)} />
-      <br />
-      <SignButton value="회원가입" handleEvent={() => create("users", userData)} />
-      <br />
-      <SignButton value="아이디 목록" handleEvent={() => read("users")} />
-    </>
+      {/* <SignButton value="아이디 목록" handleEvent={() => read("users")} /> */}
+    </SignContents>
   );
 }
 
