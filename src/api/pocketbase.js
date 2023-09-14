a20d9ec32d361bd107b69be87304c60399bf3e82
bigCoDult <qqqqmmss11@gmail.com>
2023-09-14 09:41:47 +0900

    modify : setLogIn 함수를 api로 이동

--- src/api/pocketbase.js
+++ src/api/pocketbase.js
@@ -2,15 +2,14 @@ import PocketBase from "pocketbase";
 
 const pb = new PocketBase(import.meta.env.VITE_PB_URL);
 pb.autoCancellation(false);
-export default pb
-
+export default pb;
 
 export async function create(collection, data) {
   const create = await pb.collection(collection).create(data);
-  
+
   // 추후 삭제해야 함
   console.log("created data on collection");
-  
+
   return create;
 }
 
@@ -19,25 +18,34 @@ export async function read(collection, field, id) {
     fields: field,
   });
   const collectionData = await pb.collection(collection).getList(1, 10);
-  
+
   // 추후 삭제해야 함
   console.log(field ? FieldData : collectionData);
-  
+
   return field ? FieldData : collectionData;
 }
 
 export function update(collection, itemId, data) {
   const update = pb.collection(collection).update(itemId, data);
-  
+
   // 추후 삭제해야 함
   console.log("created data on collection");
-  
-  return update
+
+  return update;
+}
+
+export async function setLogIn(idPw) {
+  const authData = await pb.collection("users").authWithPassword(...idPw);
+
+  // 추후 삭제해야 함
+  console.log(pb.authStore.model.nickname);
+
+  return authData;
 }
 
 export function setLogOut() {
   pb.authStore.clear();
-  
+
   // 추후 삭제해야 함
   console.log("Logout");
 }
