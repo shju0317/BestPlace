9ce61242afcbc1031d4244e70fb1c36174e253bc
bigCoDult <qqqqmmss11@gmail.com>
2023-09-15 20:18:58 +0900

    feat : navigate 하기위해 비로그인 컴포넌트에 Register 추가

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,15 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
+import { pb } from "@/api/pocketbase";
 import RootLayout from "@l/RootLayout";
 import NotFound from "@p/NotFound";
 import Feed from "@p/Feed";
 import ReviewWrite from "@p/ReviewWrite";
 import Region from "@p/Region";
 import ReviewList from "@p/ReviewList";
-import { pb } from "@/api/pocketbase";
-
-// import Login from "@p/Login";
-import Login from "@p/TestLogin";
-// import Login from "@p/Register";
+import Login from "@p/Login";
+import Register from "@p/Register";
 
 let isValidUser = pb.authStore.isValid;
 
@@ -31,7 +29,11 @@ const routerConfig = isValidUser
   : [
       {
         path: "/",
-        element: <Login />,
+        children: [
+          { index: true, element: <Login /> },
+          { path: "login", element: <Login /> },
+          { path: "register", element: <Register /> },
+        ],
       },
     ];
 
