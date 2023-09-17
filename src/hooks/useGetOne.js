cb8b0a14f90e7b1b91d45b9c894cdad6224e5c96
Engsheet <xiahtic73@gmail.com>
2023-09-17 20:25:39 +0900

    delete : useGetOne() 훅 삭제

--- src/hooks/useGetOne.js
+++
@@ -1,7 +0,0 @@
-import PocketBase from "pocketbase";
-
-const pb = new PocketBase("https://lionplace-db.pockethost.io");
-
-export async function useGetOne(collection, userId) {
-  return await pb.collection(collection).getOne(userId);
-}
