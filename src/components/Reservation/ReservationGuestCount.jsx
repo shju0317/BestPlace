e7e053e5a6daa1d363b3cbcd57b74a38d6f2097e
shju0317 <shju0317@naver.com>
2023-09-19 17:04:46 +0900

    feat: '등록하기' 버튼 클릭 시 데이터 전송 - guestCount 데이터 (#76)

--- src/components/Reservation/ReservationGuestCount.jsx
+++ src/components/Reservation/ReservationGuestCount.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import useReservation from '@h/useReservation';
 
 function ReservationGuestCount() {
-  const {handleInputChange, reservationData, setReservationData} = useReservation();
+  const {setReservationData} = useReservation();
   const [guestCount, setGuestCount] = useState(1);
 
   const handleGuestCountChange = (e) => {
@@ -12,7 +12,7 @@ function ReservationGuestCount() {
   };  
 
   useEffect(() => {
-    console.log(guestCount);
+    setReservationData({ "guestCount": guestCount });
     },[guestCount]
   )
 
@@ -22,19 +22,8 @@ function ReservationGuestCount() {
         <label htmlFor="guestCount" className="text-lg font-semibold">인원</label>
         <input type="number" min="1" max="10" id="guestCount"
           inputMode="numeric"
-          onChange={(e)=>{
-            handleGuestCountChange(e);
-            handleInputChange(e);
-          }}
-          className="rounded border border-primary px-4 py-2"/>
-        {/* <select id="guestCount" value={guestCount} 
           onChange={handleGuestCountChange}
-          className="rounded border border-primary px-4 py-2"
-          >
-          {Array.from({ length: 10 }, (_, index) => (
-            <option key={index + 1} value={index + 1}>{index + 1}</option>
-          ))}
-        </select> */}
+          className="rounded border border-primary px-4 py-2"/>
         <span>명</span>
       </div>
     </>
