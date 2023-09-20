8b4cfd461fcbcf2b515157fd50679abffce287e7
민규 <xiahtic73@gmail.com>
2023-09-20 09:16:31 +0900

    feat : 더보기 구현

--- src/pages/Reservation.jsx
+++ src/pages/Reservation.jsx
@@ -1,107 +1,165 @@
 import { pb } from "@/api/pocketbase";
-import SwiperCategory from "@/components/SwiperCategory";
-import { BsCalendarWeek, BsChevronDown } from "react-icons/bs";
-import { MdFoodBank, MdOutlineCheck } from "react-icons/md";
-import { PiCalendarCheckBold, PiCalendarXBold, PiNumberSquareOneFill } from "react-icons/pi";
-import { array } from "prop-types";
 import Spinner from "@/components/Spinner";
+import SwiperCategory from "@/components/SwiperCategory";
 import useReservationList from "@/hooks/useReservationList.js";
 import { dateFormat, timeFormat } from "@/utils";
+import { array, bool, func, string } from "prop-types";
+import { useState } from "react";
+import { BsCalendarWeek, BsChevronDown, BsChevronUp } from "react-icons/bs";
+import { MdFoodBank, MdOutlineCheck } from "react-icons/md";
+import { PiCalendarCheckBold, PiCalendarXBold } from "react-icons/pi";
 import { calcDay } from "./../utils/getDate";
 
 /* -------------------------------------------------------------------------- */
 
 //@ 현재 예약중 리스트
-function ReservedList({ reservedList }) {
+function ReservedList({ reservedList, nickname }) {
   return (
-    <ul>
-      {reservedList.map((item, index) => (
-        <li key={index} className="my-5 rounded-2xl border-2 border-primary p-4">
-          <p className="inline rounded-xl bg-primary px-1.5 py-0.5 text-xs font-bold text-white">
-            {calcDay(item.date)}
-          </p>
-          <h4 className="mt-1 font-bold">{item.expand.place.title}</h4>
-          <p className="text-sm font-light">
-            {dateFormat(item.date)} <span className="mx-1 font-normal opacity-40">|</span>
-            {timeFormat(item.date)}
-          </p>
-        </li>
-      ))}
-    </ul>
+    <div className="border-b border-dashed border-gray-500/50 pb-6">
+      <h3 className="mb-4 mt-2 text-lg font-bold">
+        <BsCalendarWeek className="mr-2 inline align-bottom text-3xl" />
+        <span className="mx-0.5 text-secondary">{nickname}</span>님이 현재 예약한 정보에요
+      </h3>
+
+      <ul>
+        {reservedList.map((item, index) => (
+          <li key={index} className="my-2 rounded-2xl border-2 border-primary p-4">
+            <p className="inline rounded-xl bg-primary px-1.5 py-0.5 text-xs font-bold text-white">
+              {calcDay(item.date)}
+            </p>
+            <h4 className="mt-1 font-bold">{item.expand.place.title}</h4>
+            <p className="text-sm font-light">
+              {dateFormat(item.date)} <span className="mx-1 font-normal opacity-40">|</span>
+              {timeFormat(item.date)}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
 ReservedList.propTypes = {
   reservedList: array,
+  nickname: string,
 };
 
 /* -------------------------------------------------------------------------- */
 
 //@ 예약 횟수 컴포넌트
-function ReservationCount() {
-  return (
-    <div className="">
-      <div>
-        <div className="flex justify-between">
-          <h4 className="font-semibold">
-            <PiNumberSquareOneFill className="mr-1 inline align-top text-2xl" /> 상호명
-          </h4>
-          <p className="font-bold">27회</p>
-        </div>
-        <div className="mt-3 h-1 w-full rounded-md bg-gray-200">
-          <div className="h-1 w-[54%] rounded-md bg-primary" />
-        </div>
-      </div>
+function ReservationCount({ nickname, visitedList }) {
+  const [isSeeMore, setIsSeeMore] = useState(false);
+  let renderList = !isSeeMore ? visitedList.slice(0, 3) : visitedList.slice(0, 9);
+  // const [filterList, setFilterList] = useState(renderList);
 
-      <button type="button" className="mx-auto my-5 flex items-center gap-1">
-        <span>더보기</span> <BsChevronDown />
-      </button>
+  function handleClickButton() {
+    setIsSeeMore(!isSeeMore);
+  }
+
+  return (
+    <div className="my-12">
+      <h3 className="my-5 text-lg font-bold">
+        <MdOutlineCheck className="mr-2 inline align-bottom text-3xl" />
+        <span className="mx-0.5 text-secondary">{nickname}</span>님은 LION PLACE로
+        <span className="text-secondary"> {visitedList?.length}회 </span>
+        예약했어요
+      </h3>
+
+      <ol>
+        {renderList?.map((item, index) => (
+          <li key={index} className="mb-6">
+            <div className="flex items-center justify-between">
+              <span className="mr-2 w-6 items-center rounded-lg border-2 bg-black text-center text-sm font-bold text-white">
+                {index + 1}
+              </span>
+              <h4 className="grow font-semibold">{item.expand.place.title}</h4>
+              <p className="font-bold">27회</p>
+            </div>
+            <div className="mt-2 h-1 w-full rounded-md bg-gray-200">
+              <div className={`h-1 w-[50%] rounded-md bg-primary`} />
+              {/* ${Math.floor((2 / filterList?.length) * 100)} */}
+            </div>
+          </li>
+        ))}
+      </ol>
+
+      <label
+        htmlFor="seeMoreButton"
+        className={`mx-auto my-5 flex cursor-pointer items-center justify-center gap-1 ${
+          renderList.length < 3 ? "hidden" : ""
+        }`}
+      >
+        <input type="checkbox" className="sr-only" onChange={handleClickButton} id="seeMoreButton" />
+        <span>{!isSeeMore ? "더보기" : "접기"}</span>
+        {!isSeeMore ? <BsChevronDown /> : <BsChevronUp />}
+      </label>
     </div>
   );
 }
+
+ReservationCount.propTypes = {
+  nickname: string,
+  visitedList: array,
+};
 
 /* -------------------------------------------------------------------------- */
 
-// 예약방문 아이콘
-function ReservationVisitIcon() {
-  return (
-    <div className="flex h-12 w-12 items-center justify-center rounded-full bg-primary">
-      <PiCalendarCheckBold className="text-3xl text-white" />
-    </div>
-  );
-}
-
-// 예약취소 아이콘
-function ReservationCancelIcon() {
-  return (
-    <div className="flex h-12 w-12 items-center justify-center rounded-full bg-gray-500">
-      <PiCalendarXBold className="text-3xl text-white" />
-    </div>
-  );
-}
-
 //@ 예약 리스트
-function ReservationList() {
+function ReservationList({ progressList, visitedList, canceledList }) {
+  let renderList = progressList;
+  let filter = "all";
+
+  function ReservationVisitIcon() {
+    return (
+      <div className="flex h-12 w-12 items-center justify-center rounded-full bg-primary">
+        <PiCalendarCheckBold className="text-3xl text-white" />
+      </div>
+    );
+  }
+
+  // 예약취소 아이콘
+  function ReservationCancelIcon() {
+    return (
+      <div className="flex h-12 w-12 items-center justify-center rounded-full bg-gray-500">
+        <PiCalendarXBold className="text-3xl text-white" />
+      </div>
+    );
+  }
+
   return (
     <>
-      <div className="flex gap-2">
-        <ReservationVisitIcon />
-        <div>
-          <h4 className="font-bold">상호명</h4>
-          <p className="text-sm font-light">
-            22. 12. 24 토 <span className="mx-1 font-normal opacity-40">|</span> 오후 12:00
-          </p>
-        </div>
-      </div>
-      <div className="my-4 w-full rounded-2xl border border-gray-200/50 px-4 shadow-md">
-        <div className="border-b border-dashed pb-3 pt-4">
-          <p className="font-semibold">15번째, 35일만에 예약</p>
-        </div>
+      <button type="button">전체</button>
+      <button type="button">방문</button>
+      <button type="button">예약 취소</button>
+      <button type="button" className="flex items-center">
+        기간 선택
+        <BsCalendarWeek className="ml-2 text-primary"/>
+      </button>
 
-        <div className="pb-4 pt-3">
-          <p className="font-semibold">상호명</p>
-        </div>
-      </div>
+      <ul>
+        {renderList.map((item, index) => (
+          <li key={index} className="my-8">
+            <div className="flex gap-2">
+              {!item.canceled ? <ReservationVisitIcon /> : <ReservationCancelIcon />}
+              <div>
+                <h4 className="font-bold">{item.expand.place.title}</h4>
+                <p className="text-sm font-light">
+                  {dateFormat(item.date)} <span className="mx-1 font-normal opacity-40">|</span> {timeFormat(item.date)}
+                </p>
+              </div>
+            </div>
+            <div className="my-2 w-full rounded-2xl border border-gray-200/50 px-4 shadow-md">
+              <div className="border-b border-dashed pb-3 pt-4">
+                <p className="font-semibold">15번째, 35일만에 예약</p>
+              </div>
+              <div className="pb-4 pt-3">
+                <p className="font-semibold text-gray-600">{item.expand.place.category}</p>
+                <p className="font-bold">{item.expand.place.title}</p>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
 
       <button
         type="button"
@@ -113,23 +171,31 @@ function ReservationList() {
   );
 }
 
+ReservationList.propTypes = {
+  progressList: array,
+  visitedList: array,
+  canceledList: array,
+};
+
 /* -------------------------------------------------------------------------- */
 
 //@ 예약 페이지 컴포넌트
 function Reservation() {
+  let nickname = pb.authStore.model.nickname;
   const { data: reservation, isLoading } = useReservationList();
-  let count = reservation?.length;
   let reservedList = [];
+  let progressList = [];
   let visitedList = [];
   let canceledList = [];
-  let name = pb.authStore.model.nickname;
 
   reservation?.forEach((item) => {
-    item.canceled === true
-      ? (canceledList = [...canceledList, item])
-      : item.visited === true
-      ? (visitedList = [...visitedList, item])
-      : (reservedList = [...reservedList, item]);
+    !item.canceled && !item.visited
+      ? (reservedList = [...reservedList, item])
+      : (progressList = [...progressList, item]);
+  });
+
+  progressList.forEach((item) => {
+    !item.canceled ? (visitedList = [...visitedList, item]) : (canceledList = [...canceledList, item]);
   });
 
   if (isLoading) return <Spinner />;
@@ -137,32 +203,21 @@ function Reservation() {
   return (
     <div>
       {/* 현재 예약중 리스트 */}
-      <h3 className="my-5 text-lg font-bold">
-        <BsCalendarWeek className="mr-2 inline align-bottom text-3xl" />
-        <span className="mx-0.5 text-secondary">{name}</span>님이 현재 예약한 정보에요
-      </h3>
-      <ReservedList reservedList={reservedList} />
+      <ReservedList nickname={nickname} reservedList={reservedList.reverse()} />
       {/* 카테고리 선택 */}
-      <h3 className="mb-2 text-lg font-bold">
-        <MdFoodBank className="mr-2 inline align-bottom text-3xl" />
-        카테고리를 선택하세요
-      </h3>
-      {/* 스와이퍼 카테고리 */}
-      <SwiperCategory />
+      <div className="my-6">
+        <h3 className="mb-2 text-lg font-bold">
+          <MdFoodBank className="mr-2 inline align-bottom text-3xl" />
+          카테고리를 선택하세요
+        </h3>
+        <SwiperCategory />
+      </div>
 
       {/* 예약 횟수 */}
-      <h3 className="my-5 text-lg font-bold">
-        <MdOutlineCheck className="mr-2 inline align-bottom text-3xl" />
-        <span className="text-secondary">{name}</span>님은 LION PLACE로
-        <span className="text-secondary"> {count}회 </span>
-        예약했어요
-      </h3>
-
-      {/* 예약 횟수 */}
-      <ReservationCount />
+      <ReservationCount nickname={nickname} visitedList={visitedList} />
 
       {/* 예약 리스트 */}
-      <ReservationList />
+      <ReservationList progressList={progressList} visitedList={visitedList} canceledList={canceledList} />
     </div>
   );
 }
