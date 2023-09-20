0f712374d75ccf3836bef9a0a740170e3d0be9f9
민규 <xiahtic73@gmail.com>
2023-09-21 00:04:22 +0900

    feat : 재예약 및 리뷰 작성 텍스트 추가 (링크 연결 필요) useFetchWriteReview 훅 추가 -로그인된 사용자로 필터링하여 작성된 리뷰 id 호출

---
+++ src/hooks/useFetchWriteReview.js
@@ -0,0 +1,33 @@
+import { pb } from "@/api/pocketbase";
+import { useQuery } from "@tanstack/react-query";
+
+function useFetchAllReviews() {
+  const userInfo = pb.authStore.model;
+
+  async function fetchWriteReviewList() {
+    try {
+      const reviews = await pb.collection("reviews").getFullList({
+        filter: `writer = '${userInfo.id}'`,
+        fields: "reservation",
+      });
+      let reservationId = reviews.reduce((array, review) => {
+        return [...array, review.reservation];
+      }, []);
+
+      return reservationId;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["writeReview"],
+    queryFn: fetchWriteReviewList,
+  });
+
+  if (error) console.log(error);
+
+  return { data };
+}
+
+export default useFetchAllReviews;
