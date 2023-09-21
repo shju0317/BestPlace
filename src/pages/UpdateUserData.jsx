fcfbebf377648a1c2cd1e7f2b1eae70c87cc0093
bigCoDult <qqqqmmss11@gmail.com>
2023-09-21 15:46:03 +0900

    feat : 프로필 사진 업데이트 기능추가, 해당 페이지에 헤더 추가

--- src/pages/UpdateUserData.jsx
+++ src/pages/UpdateUserData.jsx
@@ -4,54 +4,57 @@ import { useNavigate } from "react-router-dom";
 
 import SignTitle from "@c/SignInUp/SignTitle";
 import SignInput from "@c/SignInUp/SignInput";
+import SignPhoto from "@c/SignInUp/SignPhoto";
 import SignButton from "@c/SignInUp/SignButton";
 import SignForm from "@c/SignInUp/SignForm";
 import SignContents from "@c/SignInUp/SignContents";
 import { produce } from "immer";
 import Profile from "./../components/Profile";
+import ReviewPhoto from "@/components/Review/ReviewPhoto";
 
 function Register() {
   const navigate = useNavigate();
 
-  const [nickname, setNickname] = useState();
-  const [avatar, setAvatar] = useState();
-  const [username, setUsername] = useState();
-
-  const UserDataFormat = {
-    id: "",
-    collectionName: "users",
-    collectionId: "_pb_users_auth_",
-    emailVisibility: false,
-    verified: false,
-    created: "2023-08-31 06:35:29.391Z",
-    updated: "2023-08-31 06:35:29.391Z",
-    follower: [],
-    following: [],
-    favorites: [],
-    review: [],
-    username: "id와 같음",
-    nickname: "별명",
-    email: "",
-    password: "",
-    passwordConfirm: "",
-    avatar: "",
-    regions: [],
-  };
+  const [nickname, setNickname] = useState(pb.authStore.model.nickname);
+  const [avatar, setAvatar] = useState(pb.authStore.model.avatar);
+  const [username, setUsername] = useState(pb.authStore.model.username);
 
   const updateData = {
     nickname: nickname,
-    username: username,
     avatar: avatar,
+    Username: username,
   };
+
+  let formData = new FormData();
+
+  function appendFormData() {
+    for (const [key, value] of Object.entries(updateData)) {
+      formData.append(key, value);
+    }
+  }
+  function handleUserDataUpdate() {
+    appendFormData();
+    update("users", pb.authStore.model.id, formData);
+    navigate("/")
+  }
 
   return (
     <div className="flex flex-col gap-20 py-10">
       <SignTitle value="회원정보 업데이트" />
 
       <SignForm>
+        <SignPhoto
+          labelValue="프로필 사진"
+          ariaText="새 프로필 사진 입력창"
+          placeHolder=""
+          inputValue={setAvatar}
+          bgColor="bg-white"
+          textColor="text-black"
+          placeHolderColor="placeholder-black"
+        />
         <SignInput
-          labelValue="새 별명"
-          ariaText="이메일 입력창"
+          labelValue="별명"
+          ariaText="별명 입력창"
           placeHolder="사용할 새 별명을 입력하세요"
           inputValue={setNickname}
           bgColor="bg-white"
@@ -59,16 +62,7 @@ function Register() {
           placeHolderColor="placeholder-black"
         />
         <SignInput
-          labelValue="새 프로필 사진"
-          ariaText="아이디 입력창"
-          placeHolder="클릭하면 사진입력하는 기능 추가할것"
-          inputValue={setAvatar}
-          bgColor="bg-white"
-          textColor="text-black"
-          placeHolderColor="placeholder-black"
-        />
-        <SignInput
-          labelValue="새 아이디"
+          labelValue="아이디"
           ariaText="아이디 입력창"
           placeHolder="로그인에 사용할 새 아이디"
           inputValue={setUsername}
@@ -82,10 +76,10 @@ function Register() {
         <SignButton value="취소" handleEvent={() => navigate("/Feed")} bgColor="bg-white" textColor="text-red-600" />
         <SignButton
           value="수정완료"
-          handleEvent={() => update("users", pb.authStore.model.id, updateData)}
+          handleEvent={() => handleUserDataUpdate()}
           bgColor="bg-primary"
           textColor="text-white"
-        />
+          />
       </div>
     </div>
   );
