008c8e61d42c5eff5958ef109c4a6b9a20c94f6b
cfgop23 <cfgop23@gmail.com>
2023-09-24 18:37:07 +0900

    add : 접근성 검사 패키지 설치

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,25 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "@/style/tailwind.css";
 
+export async function autoA11yTest({ locale = "ko", timeout = 1000 } = {}) {
+  if (process.env.NODE_ENV.includes("development")) {
+    try {
+      const { default: axe } = await import("@axe-core/react");
+      let lang = null;
+      if (locale !== "en") {
+        lang = await import(`axe-core/locales/ko.json`);
+        // 언어 설정(영어: en, 한글: ko)
+      }
+      axe(React, ReactDOM, timeout, lang ? { locale: lang } : {});
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+}
+
+// 한국어 출력
+autoA11yTest();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
