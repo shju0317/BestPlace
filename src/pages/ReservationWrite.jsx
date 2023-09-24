7490fb384a238624d59928a49c994629c6698d58
shju0317 <shju0317@naver.com>
2023-09-24 11:02:06 +0900

    feat: 현재 시간보다 이전 시간 선택시 알림(#76)

--- src/pages/ReservationWrite.jsx
+++ src/pages/ReservationWrite.jsx
@@ -9,7 +9,7 @@ import PlaceInfo from "@c/Reservation/PlaceInfo";
 import ScrollToTop from "@c/ScrollTop";
 import Button from "@c/Button";
 import useReservation from "@h/useReservation";
-
+import { isBefore } from "date-fns/esm";
 
 
 function ReservationWrite() {
@@ -36,13 +36,17 @@ function ReservationWrite() {
       case !isValid(reservationData):
         alertMessage("필수사항을 입력해주세요.", "❗");
         return;
+      
+      case !isTelRegValid(reservationData.tel):
+        alertMessage("전화번호 정보가 올바르지 않습니다.", "❗");
+        return;
     
       case !isEmailRegValid(reservationData.email):
         alertMessage("이메일 정보가 올바르지 않습니다.", "❗");
         return;
-    
-      case !isTelRegValid(reservationData.tel):
-        alertMessage("전화번호 정보가 올바르지 않습니다.", "❗");
+        
+      case isBefore(reservationData.date, new Date()): // 현재 시간보다 이전 시간 선택 시 알림
+        alertMessage("지난 시간입니다. 시간을 확인해주세요.", "❗");
         return;
     }
     
